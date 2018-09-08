b7214ae31e7f40b59ec8ad4d7df8a03277fefbcb
Yvan Volochine <yvan.github@gmail.com>
2018-09-08 18:52:24 +0200

    trav2

--- .travis/maybe-skip-travis.sh
+++ .travis/maybe-skip-travis.sh
@@ -4,8 +4,7 @@ set -e
 MODIFIED_FILES=$(git diff --name-only $TRAVIS_COMMIT_RANGE)
 if ! echo ${MODIFIED_FILES} | grep -qvE '(\.md$)|(^examples)/'; then
   echo "Only docs were updated, stopping build process."
-  travis_terminate 0
   exit 1
 fi
 echo "BUILDING"
-exit
+exit 0
